0d6e73cdd54a6eac3d19274c566b4370d3a21c26
javier.arias <javier.arias@skyscanner.net>
2019-04-06 09:28:47 +0200

    update proto descriptor playground with new samples and descriptors

--- src/main/java/io/protovolution/ProtoDescriptorPlayground.java
+++ src/main/java/io/protovolution/ProtoDescriptorPlayground.java
@@ -10,27 +10,22 @@ import java.nio.file.Files;
 public class ProtoDescriptorPlayground {
 
     public static void main(String[] args) throws IOException, DescriptorValidationException {
-//        https://github.com/os72/protoc-jar
-//        java -jar protoc-jar-3.7.0.1.jar -v3.7.0 -o yuyuyeah --proto_path /Users/javierarias/mywork/streaming-quote-pipeline/quote-wormhole/vendor/proto/engines/quotepipelineschema quote.proto
-//        https://deliveroo.engineering/2019/02/05/improving-stream-data-quality-with-protobuf-schema-validation.html
-        // https://codeburst.io/using-dynamic-messages-in-protocol-buffers-in-scala-9fda4f0efcb3
-//        https://developers.google.com/protocol-buffers/docs/techniques#self-description
-        String x = "/Users/javierarias/mywork/streaming-quote-pipeline/quote-wormhole/vendor/proto/engines/quotepipelineschema/quote.proto";
-        String pathname = "/Users/javierarias/Downloads/yuyuyeah";
-        String pathnameRes = "/Users/javierarias/Downloads/yuyuyeahres";
-        byte[] bytes = Files.readAllBytes(new File(pathname).toPath());
-        byte[] bytesRes = Files.readAllBytes(new File(pathnameRes).toPath());
-        FileDescriptorSet fds = FileDescriptorSet.parseFrom(bytes);
-        FileDescriptorSet fdsRes = FileDescriptorSet.parseFrom(bytesRes);
+        String personV1Path = "samples/person.v1.desc";
+        String personV2Path = "samples/person.v2.desc";
+        byte[] personV1Bytes = Files.readAllBytes(new File(personV1Path).toPath());
+        byte[] personV2Bytes = Files.readAllBytes(new File(personV2Path).toPath());
+        FileDescriptorSet personV1FileDescriptorSet = FileDescriptorSet.parseFrom(personV1Bytes);
+        FileDescriptorSet personV2FileDescriptorSet = FileDescriptorSet.parseFrom(personV2Bytes);
 
-        FieldDescriptorProto field2 = fds.getFile(0)
-                .getMessageTypeList().get(0).getFieldList().get(1);
-        FieldDescriptorProto field2Res = fdsRes.getFile(0)
-                .getMessageTypeList().get(0).getFieldList().get(1);
+        FieldDescriptorProto field3PersonV1 = personV1FileDescriptorSet.getFile(0)
+                .getMessageTypeList().get(0).getFieldList().get(2);
+        FieldDescriptorProto field3PersonV2 = personV2FileDescriptorSet.getFile(0)
+                .getMessageTypeList().get(0).getFieldList().get(2);
 
 
 
-        System.out.println(field2);
+        System.out.println(field3PersonV1);
+        System.out.println(field3PersonV2);
     }
 
 }
